dcb101f070719d7735e051f1b2d2cfbc1d9f0702
Tsz Wai Ko <47970742+kenko911@users.noreply.github.com>
2026-04-13 13:56:47 -0400

    Change the stress implementation to avoid lazywrapper error
    Refactor volume calculation using cross product for better accuracy.

--- mace/modules/utils.py
+++ mace/modules/utils.py
@@ -58,7 +58,12 @@ def compute_forces_virials(
     stress = torch.zeros_like(displacement)
     if compute_stress and virials is not None:
         cell = cell.view(-1, 3, 3)
-        volume = torch.linalg.det(cell).abs().unsqueeze(-1)
+
+        a = cell[:, 0, :]
+        b = cell[:, 1, :]
+        c = cell[:, 2, :]
+
+        volume = torch.abs(torch.sum(a * torch.cross(b, c, dim=-1), dim=-1, keepdim=True))
         stress = virials / volume.view(-1, 1, 1)
         stress = torch.where(torch.abs(stress) < 1e10, stress, torch.zeros_like(stress))
     if forces is None:
